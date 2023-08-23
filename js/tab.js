a1349cab57d61550cbf741efe104e248aa5b8e31
Gwan1 <jeonggwanil@gmail.com>
2023-08-23 15:25:56 +0900

    탭기능 만들기 (for 반복문)

---
+++ js/tab.js
@@ -0,0 +1,13 @@
+// 버튼0 누르면
+// - 모든버튼에 붙은 orange 클래스명 제거
+// - 버튼0에 orange 클래스명 추가
+// - 모든 div에 붙은 show 클래스명 제거
+// - div0에 show 클래스명 추가
+
+$('.tab-button')
+  .eq(0)
+  .on('click', function () {});
+
+$('.tab-button')
+  .eq(0)
+  .click(function () {});
