7b95805983bcf2aad5a0ca222db71a74ab734a67
Gwan1 <jeonggwanil@gmail.com>
2024-06-24 23:12:37 +0900

    검색 기능 추가

--- js/store.js
+++ js/store.js
@@ -6,14 +6,43 @@ $.get('/store.json').done((data) => {
   console.log(data.products);
 
   data.products.forEach((a) => {
-    $('.row').append(`<div class="col-md-3" id="card">
+    $('.product-lis').append(`<div class="col-md-3" id="card">
           <div class="card-body">
             <img src="/img/${a.photo}" class="w-100" />
             <h5>${a.title}</h5>
-            <p>${a.brand}</p>
+            <h5>${a.brand}</h5>
             <p>가격 : ${a.price}</p>
             <a href="#" class="btn btn-primary">담기</a>
           </div>
         </div>`);
+  });
+});
+
+$('#search').on('input', function () {
+  let 검색어 = $('#search').val();
+
+  let newProducts = products.filter((a) => {
+    return a.title.includes(검색어) || a.brand.includes(검색어);
+  });
+
+  $('.row').html('');
+  newProducts.forEach((a, i) => {
+    $('product-lis').append(`<div class="col-md-3" id="card">
+          <div class="card-body">
+            <img src="/img/${a.photo}" class="w-100" />
+            <h5>${a.title}</h5>
+            <h5>${a.brand}</h5>
+            <p>가격 : ${a.price}</p>
+            <a href="#" class="btn btn-primary">담기</a>
+          </div>
+        </div>`);
+  });
+
+  $('.product-lis').each(function (i, html요소) {
+    let title = html요소.innerHTML;
+    title = title.replace(검색어, `<span style="background : yellow">${검색어}</span>`);
+    console.log(title);
+
+    html요소.innerHTML = title;
   });
 });
