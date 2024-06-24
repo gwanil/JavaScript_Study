04f1621af26dbb3b87cf12c249793aa59b0ce609
Gwan1 <jeonggwanil@gmail.com>
2024-06-24 15:04:32 +0900

    store card 수정

--- js/store.js
+++ js/store.js
@@ -6,13 +6,13 @@ $.get('/store.json').done((data) => {
   console.log(data.products);
 
   data.products.forEach((a) => {
-    $('.row').append(`<div class="card col-sm-3" id="card">
+    $('.row').append(`<div class="col-md-3" id="card">
           <div class="card-body">
             <img src="/img/${a.photo}" class="w-100" />
             <h5>${a.title}</h5>
             <p>${a.brand}</p>
             <p>가격 : ${a.price}</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+            <a href="#" class="btn btn-primary">담기</a>
           </div>
         </div>`);
   });
