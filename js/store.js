f4d685acdb04b203f7fa0ff2116a537abd5fdbb2
Gwan1 <jeonggwanil@gmail.com>
2024-06-25 12:36:10 +0900

    검색기능 실행오류 수정 완료

--- js/store.js
+++ js/store.js
@@ -21,13 +21,16 @@ $.get('/store.json').done((data) => {
 $('#search').on('input', function () {
   let 검색어 = $('#search').val();
 
+  // 필터링된 제품 목록 생성
   let newProducts = products.filter((a) => {
     return a.title.includes(검색어) || a.brand.includes(검색어);
   });
 
+  // 기존 콘텐츠 초기화
   $('.row').html('');
+  // 필터링된 제품 목록 DOM에 추가
   newProducts.forEach((a, i) => {
-    $('product-lis').append(`<div class="col-md-3" id="card">
+    $('.product-lis').append(`<div class="col-md-3" id="card">
           <div class="card-body">
             <img src="/img/${a.photo}" class="w-100" />
             <h5>${a.title}</h5>
@@ -38,6 +41,7 @@ $('#search').on('input', function () {
         </div>`);
   });
 
+  // 검색된 단어 하이라이트 표시
   $('.product-lis').each(function (i, html요소) {
     let title = html요소.innerHTML;
     title = title.replace(검색어, `<span style="background : yellow">${검색어}</span>`);
